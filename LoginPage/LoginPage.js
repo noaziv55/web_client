53fb0fd25a5f757b4353e2f78ae8063e97891116
noaziv55 <92301625+noaziv55@users.noreply.github.com>
2022-04-14 17:59:38 +0300

    Additional updates

--- LoginPage/LoginPage.js
+++ LoginPage/LoginPage.js
@@ -1,40 +1,52 @@
 import './LoginPage.css';
 import { Component } from 'react';
 import React from 'react';
+import { validateUser, userDataBase} from '../DataBase/DataBase';
 
 export class LoginPage extends Component {
 
     validatefields() {
-        var username = document.getElementById("Username");
-        var password = document.getElementById("Password");
-        if (username.value.length === 0 || password.value.length === 0) {
+        var username = document.getElementById("Username").value;
+        var password = document.getElementById("Password").value;
+        if (username.length === 0 || password.length === 0) {
             alert("Please fill all fields properly!");
-            return false;
+            return;
         }
-        if (password.value.length < 8) {
+        if (password.length < 8) {
             alert("Password too short! Please enter 8 characters or more");
-            return false;
+            return;
         } else {
-            alert("SignIn successfully")
-            //todo: add user to map
+            if (userDataBase.has(username)) {
+                if(validateUser(username, password)){
+                    alert("SignIn successfully");
+                    window.location.href = "ChatPage";
+                }
+                
+            }
+            alert("user doesn't exist");
+            return;
         }
     }
 
     render() {
         return (
-            <div className="outterBlock">
-                <div className="block">
-                    <div className="form-floating very-cool-margin">
-                        <input type="username" className="form-control" id="Username" placeholder="text" required></input>
-                        <label htmlFor="floatingInput">Username</label>
+            <div className="container">
+                <div className="outterBlock">
+                    <div className="block">
+                        <div className="form-floating very-cool-margin">
+                            <input type="username" className="form-control" id="Username" placeholder="text" required></input>
+                            <label htmlFor="floatingInput">Username</label>
+                        </div>
+                        <div className="form-floating">
+                            <input type="password" className="form-control" id="Password" placeholder="Password" minLength="8" required></input>
+                            <label htmlFor="floatingPassword">Password</label>
+                        </div>
                     </div>
-                    <div className="form-floating">
-                        <input type="password" className="form-control" id="Password" placeholder="Password" minLength="8" required></input>
-                        <label htmlFor="floatingPassword">Password</label>
-                    </div>
+
+                    <button type="submit" className="btn btn-success" onClick={this.validatefields}> Login </button>
+
+                    <div className="register-link">Not registered? <a href="RegisterPage" className="link-success">Click here</a> to register.</div>
                 </div>
-                <button type="submit" className="btn btn-success" onClick={this.validatefields}> Login </button>
-                <div className="register-link">Not registered? <a href="RegisterPage" className="link-success">Click here</a> to register.</div>
             </div>
         );
     }
